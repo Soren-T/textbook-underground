0dce8c5eb70d48cd88298617162829981dcd5320
Soren Thompson <sorenct04@gmail.com>
2016-06-17 11:27:57 -0600

    update for login/passport method

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+function login (req, res){
+	return res.json({message: "to be implemented"})
+}
+function logout (req, res){
+	return res.json({message: "to be implemented"})
+}
+
+module.exports = {
+	login,
+	logout,
+}
