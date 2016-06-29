2a96855eb24592cf25e82bba819f7afce3e15750
Soren Thompson <sorenct04@gmail.com>
2016-06-29 17:14:38 -0600

    update users is working now

--- app/containers/ManageUsers.jsx
+++ app/containers/ManageUsers.jsx
@@ -7,6 +7,72 @@ import {browserHistory} from 'react-router';
 
 const cx = classNames.bind(styles);
 
+
+
+class User extends React.Component{
+    updateUser(){
+      console.log(React.findDOMNode(this.refs.email))
+    var email = React.findDOMNode(this.refs.email).value;
+    var isBlocked = React.findDOMNode(this.refs.isBlocked).value;
+    var self = this
+    console.log('is Blocked', isBlocked);
+    console.log('email', email);
+    var data = {email: email, isBlocked: isBlocked};
+
+    fetch('/api/v1/users/' + this.props.id, {
+      credentials: 'same-origin',
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      console.log('parsed json', json)
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    })
+  }
+
+  deleteUser(){
+    var self = this
+    console.log(id)
+    fetch('/api/v1/users/' + this.props._id, {
+      credentials: 'same-origin',
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }})
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      console.log('parsed json', json)
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    }).then(function() {
+      window.location.reload()
+    })
+  }
+
+  render(){
+        return (<div className={cx('buyerBookList')}>
+          <div className={cx('email')}>{this.props.email} </div>
+          <div className={cx('isBlocked')}> Block User</div>
+          <div>Current status: {this.props.isBlocked.toString()}</div>
+          <input ref='email'  /> <br/>
+          <input ref='isBlocked'/> <br/>
+          <button onClick={() => this.updateUser()}>Update User</button>
+          <button onClick={this.deleteUser.bind(this)}>Delete User</button>
+          <br/>
+        </div>)
+
+  }
+
+}
 export default class ManageUsers extends React.Component {
   constructor(props){
     super(props);
@@ -25,51 +91,9 @@ export default class ManageUsers extends React.Component {
       console.log('parsing failed', ex)
     })
   }
-  updateUser(id){
-    var email = React.findDOMNode(this.refs.email).value;
-    var isBlocked = React.findDOMNode(this.refs.isBlocked).value;
-    var self = this
-    var data = ({email: email, isBlocked: isBlocked});
 
-    fetch('/api/v1/users/' + id, {
-      credentials: 'same-origin',
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    console.log(data)
-    .then(function(response) {
-      return response.json()
-    }).then(function(json) {
-      console.log('parsed json', json)
-    }).catch(function(ex) {
-      console.log('parsing failed', ex)
-    })
-  }
 
-   deleteUser(id){
-    var self = this
-    console.log(id)
-    fetch('/api/v1/users/' + id, {
-      credentials: 'same-origin',
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }})
-    .then(function(response) {
-      return response.json()
-    }).then(function(json) {
-      console.log('parsed json', json)
-    }).catch(function(ex) {
-      console.log('parsing failed', ex)
-    }).then(function() {
-      window.location.reload()
-    })
-  }
+
   changeBlocked(blocked){
     return !blocked
   }
@@ -78,24 +102,9 @@ export default class ManageUsers extends React.Component {
   }
 
    display(){
-      var self = this
-      return this.state.user.map(function(user){
-        if(!user.local.isAdmin){
-        self.state.isBlocked =  user.local.isBlocked
-        self.state.email =  user.local.email
-          return (<div className={cx('buyerBookList')}>
-            <div className={cx('email')}>{user.local.email} </div>
-            <div className={cx('isBlocked')}> Block User</div>
-              <div>Current status: {user.local.isBlocked.toString()}</div>
-              <input ref='email'  /> <br/>
-              <input ref='isBlocked' /> <br/>
-            <button onClick={() => self.updateUser(user._id)}>Update User</button>
-            <button onClick={self.deleteUser.bind(self, user._id)}>Delete User</button>
-            <br/>
-          </div>)
-        }
-      })
-    }
+      return this.state.user.map((user) =>
+           (<User isBlocked={user.local.isBlocked} isAdmin={user.local.isAdmin} email={user.local.email} id={user._id} />)
+    )}
 
   render() {
     return (
