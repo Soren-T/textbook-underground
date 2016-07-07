bfbe2dd3269ad728bf7ec98e8bf3f59068151b5f
Soren Thompson <sorenct04@gmail.com>
2016-07-07 11:17:33 -0600

    css fixes for heroku and making font style standard across the site

--- app/containers/SellerHomepage.jsx
+++ app/containers/SellerHomepage.jsx
@@ -27,18 +27,20 @@ export default class SellerHomepage extends React.Component {
       	<h1>Seller Homepage</h1>
       		<br/>
         <div className={cx('buyerBooks')}>
-        <h3>Your current list of books for sale.</h3>
+        <h2>Your current list of books for sale.</h2>
+          <p>To add a book listing, click on the menu bar and select "Sell Your Books". Any book you list will be available for 30 days. After that date you will need to re-activate this listing.</p>
           <div>
             {this.state.books.map((book)=>(
               <div className={cx('buyerBookList')}>
-               <div className={cx('photo')}><img src={book.photo}/> </div>
-               <div className={cx('title')}>  {book.title} </div>
-               <div className={cx('author')}>by {book.author} </div>
-               <div className={cx('ISBN')}>ISBN: {book.ISBN} </div>
-               <div className={cx('price')}>${book.price} </div>
-               <div className={cx('condition')}>Condition: {book.condition} </div>
-               <div className={cx('description')}>Description: <br/> {book.description} </div>
-               <Link className={cx('editLink')} to={`/Editor/${book._id}`}>Edit Post</Link>
+                <div className={cx('photo')}><img src={book.photo}/> </div>
+                <div className={cx('title')}>  {book.title} </div>
+                <div className={cx('author')}>by {book.author} </div>
+                <div className={cx('ISBN')}>ISBN: {book.ISBN} </div>
+                <div className={cx('price')}>${book.price} </div>
+                <div className={cx('condition')}>Condition: {book.condition} </div>
+                <div className={cx('description')}>Description: <br/> {book.description} </div>
+                <br/>
+                <Link className={cx('editLink')} to={`/Editor/${book._id}`}>Edit Post</Link>
                 <br/>
               </div>
             ))} 
